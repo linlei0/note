309ee1fa6582a102c4bd4cb47c2d7fd8c9cd5e3a
linlei <1766326870@qq.com>
2020-09-28 19:04:53 +0800

    提交代码

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 
 # 个人博客
 
+记录遇到的问题，和一些学习笔记，
 
-记录遇到的问题，和一些学习笔记，还有做的一些算法题
-
-访问地址：[l-sui.github.io](https://l-sui.github.io)
+访问地址：[https://linlei0.github.io/note/](https://linlei0.github.io/note/)
 时间原因，有一部分没来得及整理--
 部分内容转载自：[hondrytravis.github.io](https://hondrytravis.github.io)
 ---
--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -10,11 +10,11 @@ const algorithm = {
     items: [
         {
             text: '每日',
-            link: 'https://github.com/L-SUI/note/tree/master/every',
+            link: '/',
         },
         {
             text: 'LeetCode',
-            link: 'https://github.com/L-SUI/note/tree/master/LeetCode/',
+            link: '/',
         }
     ]
 }
