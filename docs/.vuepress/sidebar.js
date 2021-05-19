b09675113971590fa067516091b7ed52559ac873
linlei <1766326870@qq.com>
2021-05-19 23:23:57 +0800

    删除多余目录

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -1,71 +1,71 @@
 // note
 const note = [
-    {
-        title: '性能优化',
-        collapsable: true,
-        children: [
-            'optimization/性能优化一',
-            'optimization/性能优化二',
-            'optimization/性能优化三',
-            'optimization/性能优化四',
-        ]
-    },
-    {
-        title: 'TypeScript',
-        collapsable: true,
-        children: [
-            'typescript/recursive_optimization',
-            'typescript/functional_programming.md'
-        ]
-    },
-    {
-        title: 'Node',
-        collapsable: true,
-        children: [
-            'node/node',
-            'node/pm2'
-        ]
-    },
-    {
-        title: 'HTTP',
-        collapsable: true,
-        children: [
-            'http/http_1',
-            'http/http_2'
-        ]
-    },
-    {
-        title: 'Linux',
-        collapsable: true,
-        children: [
-            'linux/linux',
-            'linux/linux_deploy',
-            'linux/linux_centos',
-            'linux/BasicCommand',
-            // 'linux/linux免密登陆',
-        ]
-    },
-    {
-        title: 'OS',
-        collapsable: true,
-        children: [
-            'os/thread'
-        ]
-    },
-    {
-        title: 'Python',
-        collapsable: true,
-        children: [
-            'python/python爬虫'
-        ]
-    },
-    {
-        title: 'Java',
-        collapsable: true,
-        children: [
-            'java/java'
-        ]
-    },
+    // {
+    //     title: '性能优化',
+    //     collapsable: true,
+    //     children: [
+    //         'optimization/性能优化一',
+    //         'optimization/性能优化二',
+    //         'optimization/性能优化三',
+    //         'optimization/性能优化四',
+    //     ]
+    // },
+    // {
+    //     title: 'TypeScript',
+    //     collapsable: true,
+    //     children: [
+    //         'typescript/recursive_optimization',
+    //         'typescript/functional_programming.md'
+    //     ]
+    // },
+    // {
+    //     title: 'Node',
+    //     collapsable: true,
+    //     children: [
+    //         'node/node',
+    //         'node/pm2'
+    //     ]
+    // },
+    // {
+    //     title: 'HTTP',
+    //     collapsable: true,
+    //     children: [
+    //         'http/http_1',
+    //         'http/http_2'
+    //     ]
+    // },
+    // {
+    //     title: 'Linux',
+    //     collapsable: true,
+    //     children: [
+    //         'linux/linux',
+    //         'linux/linux_deploy',
+    //         'linux/linux_centos',
+    //         'linux/BasicCommand',
+    //         // 'linux/linux免密登陆',
+    //     ]
+    // },
+    // {
+    //     title: 'OS',
+    //     collapsable: true,
+    //     children: [
+    //         'os/thread'
+    //     ]
+    // },
+    // {
+    //     title: 'Python',
+    //     collapsable: true,
+    //     children: [
+    //         'python/python爬虫'
+    //     ]
+    // },
+    // {
+    //     title: 'Java',
+    //     collapsable: true,
+    //     children: [
+    //         'java/java'
+    //     ]
+    // },
     {
         title: 'Nginx',
         collapsable: true,
@@ -73,79 +73,95 @@ const note = [
             'nginx/nginx'
         ]
     },
-    {
-        title: '项目工程化',
-        collapsable: true,
-        children: [
-            'engineering/ci_cd',
-            'engineering/sonar',
-        ]
-    },
-    {
-        title: '数据库',
-        collapsable: true,
-        children: [
-            'mysql/mysql'
-        ]
-    },
-    {
-        title: '常用工具',
-        collapsable: true,
-        children: [
-            'mac/command_line',
-            'mac/homebrew',
-            'mac/nvm',
-            'node/nrm'
-        ]
-    },
-    {
-        title: '自动化',
-        collapsable: true,
-        children: [
-            'automation/jenkins',
-            'automation/cli',
-        ]
-    },
-    {
-        title: 'js相关',
-        collapsable: true,
-        children: [
-            'jsNote/base/函数柯里化curry',
-            'jsNote/base/继承',
-            'jsNote/base/使用setTimeout模拟setInterval',
-            'jsNote/base/手写防抖(Debouncing)和节流(throttling)',
-            'jsNote/base/手写实现拖拽',
-            'jsNote/base/手写实现Object.create',
-            'jsNote/base/手写一个call或apply',
-            'jsNote/base/手写一个Function.bind',
-            'jsNote/base/手写一个instanceOf原理',
-            'jsNote/base/手写一个JS深拷贝(由浅入深多种解法)',
-            'jsNote/base/手写一个JSON.parse和JSOn.stringify',
-            'jsNote/base/手写一个map和reduce',
-            'jsNote/base/手写一个new操作符',
-            'jsNote/base/eventloop',
-        ]
-    },
-    {
-        title: 'QA相关',
-        collapsable: true,
-        children: [
-            'QA/JavaScript&QA',
-        ]
-    },
-    {
-        title: 'CSS',
-        collapsable: true,
-        children: [
-            "css/现代化CSS方法论"
-        ]
+    // {
+    //     title: '项目工程化',
+    //     collapsable: true,
+    //     children: [
+    //         'engineering/ci_cd',
+    //         'engineering/sonar',
+    //     ]
+    // },
+    // {
+    //     title: '数据库',
+    //     collapsable: true,
+    //     children: [
+    //         'mysql/mysql'
+    //     ]
+    // },
+    // {
+    //     title: '常用工具',
+    //     collapsable: true,
+    //     children: [
+    //         'mac/command_line',
+    //         'mac/homebrew',
+    //         'mac/nvm',
+    //         'node/nrm'
+    //     ]
+    // },
+    // {
+    //     title: '自动化',
+    //     collapsable: true,
+    //     children: [
+    //         'automation/jenkins',
+    //         'automation/cli',
+    //     ]
+    // },
+    // {
+    //     title: 'js相关',
+    //     collapsable: true,
+    //     children: [
+    //         'jsNote/base/函数柯里化curry',
+    //         'jsNote/base/继承',
+    //         'jsNote/base/使用setTimeout模拟setInterval',
+    //         'jsNote/base/手写防抖(Debouncing)和节流(throttling)',
+    //         'jsNote/base/手写实现拖拽',
+    //         'jsNote/base/手写实现Object.create',
+    //         'jsNote/base/手写一个call或apply',
+    //         'jsNote/base/手写一个Function.bind',
+    //         'jsNote/base/手写一个instanceOf原理',
+    //         'jsNote/base/手写一个JS深拷贝(由浅入深多种解法)',
+    //         'jsNote/base/手写一个JSON.parse和JSOn.stringify',
+    //         'jsNote/base/手写一个map和reduce',
+    //         'jsNote/base/手写一个new操作符',
+    //         'jsNote/base/eventloop',
+    //     ]
+    // },
+    // {
+    //     title: 'QA相关',
+    //     collapsable: true,
+    //     children: [
+    //         'QA/JavaScript&QA',
+    //     ]
+    // },
+    // {
+    //     title: 'CSS',
+    //     collapsable: true,
+    //     children: [
+    //         "css/现代化CSS方法论"
+    //     ]
 
+    // },
+    {
+        title: 'JavaScript',
+        collapsable: true,
+        children: [
+            'JavaScript/手写系列',
+            'JavaScript/继承'
+        ]
+    },
+    {
+        title: 'html&浏览器',
+        collapsable: true,
+        children: [
+            'html&浏览器/缓存策略'
+        ]
     },
     {
         title: 'webpack',
         collapsable: true,
         children: [
-            "webpack/基本使用"
+            "webpack/基本使用",
+            "webpack/工作原理"
         ]
     },
     {
--- docs/note/nginx/nginx.md
+++ docs/note/nginx/nginx.md
@@ -1,185 +1,71 @@
-# Nginx 初探
+# Nginx
+## Nginx安装步骤
+### 一、安装编译工具及库文件
+`yum -y install make zlib zlib-devel gcc-c++ libtool  openssl openssl-devel`
+### 二、首先要安装 PCRE
+1. 下载 PCRE 安装包
 
-## 配置文件
+`cd /usr/local/src/`
 
-配置文件在 `/usr/local/etc/nginx` 中设置 `config`
+`wget http://downloads.sourceforge.net/project/pcre/pcre/8.35/pcre-8.35.tar.gz`
 
-```bash
-# For more information on configuration, see:
-#   * Official English Documentation: http://nginx.org/en/docs/
-#   * Official Russian Documentation: http://nginx.org/ru/docs/
+2. 解压安装包
 
-user nginx;
+`tar zxvf pcre-8.35.tar.gz`
 
-# 工作进程=>一般是 cpu 有几核就写几,可以最大限度的去发挥它的性能
-worker_processes auto;
+3. 进入安装包目录
 
-# 错误日志路径
-error_log /var/log/nginx/error.log;
+`cd pcre-8.35`
 
-# 一般不动它,给守护进程用的
-pid /run/nginx.pid;
+4. 编译安装 
 
-# Load dynamic modules. See /usr/share/doc/nginx/README.dynamic.
-include /usr/share/nginx/modules/*.conf;
+`./configure`
 
-# 并发连接数=>最大并发数:一个工作进程下的最大连接;默认 1024 个
-events {
-    worker_connections 1024;
-}
+`make && make install`
 
-# http 配置
-http {
-    # 日志格式
-    log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
-                      '$status $body_bytes_sent "$http_referer" '
-                      '"$http_user_agent" "$http_x_forwarded_for"';
+5. 查看pcre版本
 
-    access_log  /var/log/nginx/access.log  main;
-    # 是否允许上传文件
-    sendfile            on;
-    # 能不能推送
-    tcp_nopush          on;
-    # gzip 压缩
-    gzip                on;
-    tcp_nodelay         on;
-    # 长连接多长时间没有通信自动断开
-    keepalive_timeout   65;
-    types_hash_max_size 2048;
-    include             /etc/nginx/mime.types;
-    # 默认的 http 头
-    default_type        application/octet-stream;
+`pcre-config --version`
 
-    # Load modular configuration files from the /etc/nginx/conf.d directory.
-    # See http://nginx.org/en/docs/ngx_core_module.html#include
-    # for more information.
-    include /etc/nginx/conf.d/*.conf;
-    # 定义反向代理服务器
-    upstream web{
-        server 127.0.0.1:8080;
-        # 这里的 server 如果只写一个就是单纯的额外网发布,如果写 n 个就是负载均衡  
-        server 127.0.0.1:8888 weight=1; #添加权重
-    }
+### 安装 Nginx
+1. 下载 Nginx
 
-    # 一个 server 对应一个网站
-    server {
-        # 端口
-        listen       80 default_server;
-        listen       [::]:80 default_server;
-        # server域名
-        server_name  _;
-        root         /usr/share/nginx/html;
+`cd /usr/local/src/`
 
-        # Load configuration files for the default server block.
-        include /etc/nginx/default.d/*.conf;
-        # 站点根目录
-        location / {
-            root    html;
-            index   index.html  index.htm;
-        }
-        # 反向代理
-        location /xxx/ {
-            # 过来的请求代理到哪里
-            proxy_pass http://web; # 前面upstream定义的
-            # 如果需要客户端 ip,这个开关可能会重写为反向代理的 ip
-            proxy_redirect off;
-            # nginx 可能会改写头,用原来的值再把它改回来
-            proxy_set_header Hose $host;
-            # 代理服务器转发请求的时候用的协议版本
-            proxy_http_version 1.1;
+`wget http://nginx.org/download/nginx-1.18.0.tar.gz`
 
-            proxy_set_header Upgrade $http_upgrade;
-            proxy_set_header Connection 'upgrade';
-            proxy_cache_bypass $http_upgrade;
-            # 取客户端真实 ip
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+2. 解压
 
-            # 超时
-            proxy_connect_timeout 600;
-            proxy_read_timeout 600;
-        }
-        # 配置 https
-        server {
-            # 一定要带上 ssl 标记,默认 443 端口
-            listen       443 ssl;
-            server_name  work.com;
-            ssl                  on;
-            # 证书
-            ssl_certificate      /etc/nginx/server.crt;
-            # 密钥
-            ssl_certificate_key  /etc/nginx/server.key;
-            # 超时
-            ssl_session_timeout  5m;
-            location / {
-                root   /usr/local/web/;
-                add_header 'Cache-Control' 'no-store';
-            }
-            error_page 404 /404.html;
-                location = /40x.html {
-            }
+`tar zxvf nginx-1.18.0.tar.gz`
 
-            error_page 500 502 503 504 /50x.html;
-                location = /50x.html {
-            }
-        }
+3. 进入安装包目录
 
-        error_page 404 /404.html;
-            location = /40x.html {
-        }
+`cd nginx-1.18.0`
 
-        error_page 500 502 503 504 /50x.html;
-            location = /50x.html {
-        }
-    }
+4. 编译
 
-# Settings for a TLS enabled server.
-#
-#    server {
-#        listen       443 ssl http2 default_server;
-#        listen       [::]:443 ssl http2 default_server;
-#        server_name  _;
-#        root         /usr/share/nginx/html;
-#
-#        ssl_certificate "/etc/pki/nginx/server.crt";
-#        ssl_certificate_key "/etc/pki/nginx/private/server.key";
-#        ssl_session_cache shared:SSL:1m;
-#        ssl_session_timeout  10m;
-#        ssl_ciphers HIGH:!aNULL:!MD5;
-#        ssl_prefer_server_ciphers on;
-#
-#        # Load configuration files for the default server block.
-#        include /etc/nginx/default.d/*.conf;
-#
-#        location / {
-#        }
-#
-#        error_page 404 /404.html;
-#            location = /40x.html {
-#        }
-#
-#        error_page 500 502 503 504 /50x.html;
-#            location = /50x.html {
-#        }
-#    }
+`./configure`
 
-}
+`make && make install`
 
-```
+5. 查看nginx版本
 
-## 启动配置
+`cd /usr/local/nginx/sbin`
 
-启动目录在 `/usr/local/bin` 目录下启动
+`./nginx -v`
 
-## 常用 bash 命令
+6. 查看进程
 
-```bash
-    #启动命令
-    nginx
-    #停止命令
-    nginx -s quit
-    #查看进程
-    ps -ef|grep nginx
-    #终止进程
-    kill -9 [进程命令]
-```
+`ps -ef | grep nginx`
+
+## nginx常用命令
+1. 查看版本
+`./nginx -v`
+2. 查看进程
+`ps -ef | grep nginx`
+3. 启动
+`./nginx`
+4. 停止
+`./nginx -s stop`
+5. 重载
+`./nginx -s reload`
