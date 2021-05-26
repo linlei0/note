6498e363fc5c995bb72869fc0f44bc8ff5a6b9f1
linlei <1766326870@qq.com>
2021-05-26 15:12:01 +0800

    修改基础配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,9 @@
 const nav = require('./nav.js')
 const sidebar = require('./sidebar.js')
-
+// console.log(process)
 module.exports = {
     title: 'linlei space',
-    base: '/note',
+    base: '/note/', // 这个地方需要注意下
     description: '努力向前',
     markdown: {
         lineNumbers: true
